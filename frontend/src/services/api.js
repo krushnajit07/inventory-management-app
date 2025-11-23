840fd87c0aaac52505d8e3fab0cc6f8e89983f7c
krushnajit07 <krushnajit3222@gmail.com>
2025-11-23 13:54:19 +0530

    Linked to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://inventory-management-app-uo9n.onrender.com/api",
 });
 
 export default API;
