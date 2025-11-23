ba83fdbc35f826da774e3fcfe1d9f6d39b9f9f0c
krushnajit07 <krushnajit3222@gmail.com>
2025-11-23 13:18:18 +0530

    Changed port

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Inventory Management Backend Running.." });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
